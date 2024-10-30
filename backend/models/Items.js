37c6e20c84990c271ce8fcd3fa161d69a1732234
Kaidhn Shearer <kaidhnshearer@gmail.com>
2024-10-30 10:58:27 +1000

    frontend and backend setup

---
+++ backend/models/Items.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    }
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
